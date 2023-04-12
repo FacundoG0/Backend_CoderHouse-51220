0b5857f9dcbb518ac92d171d60841b923da5e85d
Facundo Gordillo <gordillofacundo44@gmail.com>
2023-04-12 09:58:44 -0300

    id filter

--- app.js
+++ app.js
@@ -7,25 +7,50 @@ const puerto = 8080;
 //Leer FS
 const fs = require('fs');
 
-let productos = async () => {
-        try{
-            const data = await fs.promises.readFile("./files/newfile.json");
-            let prods = await JSON.parse(data);
+let getallprods = async () => {
+    try{
+        const data = await fs.promises.readFile("./files/newfile.json");
+        let prods = await JSON.parse(data);
 
-            return [
-                server.get("/products", (req, res) => {
-                    res.send(prods)
-                })
-            ]
-        }
-     
-        catch(err){
-            console.log(err);
-        }
+        return [
+            server.get("/products", (req, res) => {
+                res.send(prods)
+            })
+        ]
     }
+    
+    catch(err){
+        console.log(err);
+    }
+}
 
-// EJECUCIÓN EXPRESS:
-productos();
+
+let getoneprod = async () => {
+    try{
+        const data = await fs.promises.readFile("./files/newfile.json");
+        let prods = await JSON.parse(data);
+
+        server.get("/products/:id", (req, res) => {
+
+            let founded = prods.find(data => data.id == req.params.id)
+
+            if (!founded) {
+                return res.send(`No existe producto con ID ${req.params.id}`)    
+            } else {
+                return res.send(founded)
+            }
+
+        })
+    }
+    
+    catch(err){
+        console.log(err);
+    }
+}
+
+// EJECUCIÓN:
+getallprods();
+getoneprod();
 
 server.listen(puerto, () => {
     console.log(`Servidor express escuchando en puerto ${puerto}`)
